79147e0c1b1f7df95791f5127adc9bc8050cb900
muhammadhashir786 <muhammadhashiransari12@gmail.com>
2025-12-01 12:47:43 +0500

    add

--- SQL - CWH 3 Hour/my_first_sql_script.sql
+++ SQL - CWH 3 Hour/my_first_sql_script.sql
@@ -1,11 +1,30 @@
-CREATE DATABASE startersql;
-USE startersql;
-CREATE TABLE users (
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(100) NOT NULL,
-email VARCHAR(100) UNIQUE NOT NULL,
-gender ENUM('Male', 'Female', 'Other'),
-date_of_birth DATE,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-SELECT * FROM users;  
+-- CREATE DATABASE startersql;
+
+-- USE startersql;
+
+-- CREATE TABLE users (
+-- id INT AUTO_INCREMENT PRIMARY KEY,
+-- name VARCHAR(100) NOT NULL,
+-- email VARCHAR(100) UNIQUE NOT NULL,
+-- gender ENUM('Male', 'Female', 'Other'),
+-- date_of_birth DATE,
+-- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- SELECT * FROM users;
+-- SELECT name, email FROM users; 
+
+-- DROP DATABASE startersql;
+
+-- RENAME TABLE users TO programmers; 
+-- SELECT * from programmers;
+
+-- ALTER TABLE users ADD column is_active BOOLEAN DEFAULT true;
+-- SELECT * from users;
+
+-- ALTER TABLE users DROP column is_active;
+-- SELECT * from users;
+
+-- ALTER TABLE users MODIFY COLUMN name VARCHAR(150);
+-- SELECT * from users;
+
