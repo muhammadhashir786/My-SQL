ffe0124010e1f8d49f10c6ccc234035aa26f834c
muhammadhashir786 <muhammadhashiransari12@gmail.com>
2025-12-03 22:30:02 +0500

    Add

---
+++ SQL - CWH 3 Hour/1 Shot My SQL Tutorial/file 1 create database and table.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS startersql;
+USE startersql;
+
+-- CREATE TABLE users (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     email VARCHAR(100) UNIQUE NOT NULL,
+--     gender ENUM('Male', 'Female', 'Other'),
+--     date_of_birth DATE,
+--     salary DECIMAL(10, 2),
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- SELECT * from users;
+
+-- SELECT id, email from users;
+
+SELECT gender from users;
+
